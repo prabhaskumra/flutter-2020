a2da0a7dad5e81bfcf52eece5077fa9c2ec81b3e
prabhaskumra <prabhas1998@gmail.com>
2020-05-31 18:52:55 -0700

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,12 +1,45 @@
-// Prabhas Kumra
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello World!'),);
+  void answerQuestion() {
+    print('Answer Chosen');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 chosen!'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

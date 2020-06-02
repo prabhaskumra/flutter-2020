820e48c2ac978c25ca6ec523c10c49377d59c257
prabhaskumra <prabhas1998@gmail.com>
2020-06-02 15:51:20 -0700

    Update question.dart

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -7,6 +7,10 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Text(
+      questionText,
+      style: TextStyle(fontSize: 28),
+      textAlign: TextAlign.center,
+    );
   }
 }

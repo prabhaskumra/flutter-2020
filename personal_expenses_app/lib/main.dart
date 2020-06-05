bc5a14944ee3dda912318ba7ba1f5f4c11ffb22f
prabhaskumra <prabhas1998@gmail.com>
2020-06-04 17:21:27 -0700

    Update main.dart

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -21,6 +21,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -32,6 +34,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Card(
+            color: Colors.red,
             child: Text('LIST OF TX'),
           ),
         ],

03dfaae350c96ad1f1e1f3fea84144308e9c89af
prabhaskumra <prabhas1998@gmail.com>
2020-06-11 02:29:18 -0700

    Corrected Screen Ratio

--- personal_expenses_app/lib/widgets/transaction_list.dart
+++ personal_expenses_app/lib/widgets/transaction_list.dart
@@ -10,9 +10,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transactions.isEmpty
+    return transactions.isEmpty
           ? Column(
               children: <Widget>[
                 Text(
@@ -64,7 +62,6 @@ class TransactionList extends StatelessWidget {
                 );
               },
               itemCount: transactions.length,
-            ),
     );
   }
 }

f98352bcced8b79e57f43ee52cf2b947039666df
prabhaskumra <prabhas1998@gmail.com>
2020-07-24 18:32:54 -0700

    Update orders_screen.dart

--- shop_app/lib/screens/orders_screen.dart
+++ shop_app/lib/screens/orders_screen.dart
@@ -7,46 +7,60 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/oders';
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
+  // var _isLoading = false;
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  // @override
+  // void initState() {
+  //   // Future.delayed(Duration.zero).then((_) async {
 
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  //   // _isLoading = true;
+  //   // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //   //   setState(() {
+  //   //     _isLoading = false;
+  //   //   });
+  //   // });
+
+  //   // });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('building orders!');
+    // final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Your Orders'),
-        ),
-        drawer: AppDrawer(),
-        body: _isLoading
-            ? (Platform.isIOS
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return (Platform.isIOS
                 ? Center(child: CupertinoActivityIndicator())
-                : Center(child: CircularProgressIndicator()))
-            : ListView.builder(
-                itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-                itemCount: orderData.orders.length,
-              ));
+                : Center(child: CircularProgressIndicator()));
+          } else {
+            if (dataSnapshot.error != null) {
+              // Error handling stuff
+              return Center(
+                child: Text('An error occured!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
   }
 }
